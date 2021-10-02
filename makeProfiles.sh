029132ae581743a9e92b2bea0e22600d53bd7812
Sean Marquez <capsulecorplab@gmail.com>
2021-10-02 17:50:16 -0600

    move profile src directory to profiles/
    - update makeProfiles.sh
    - update .gitignore

--- makeProfiles.sh
+++ makeProfiles.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --rm --volume "$PWD:/srv" -w "/srv" kramos/alpine-zip Ender3_v2_PETG.zip Ender3_v2_PETG/creality_* -j
+docker run --rm --volume "$PWD:/srv" -w "/srv" kramos/alpine-zip Ender3_v2_PETG.zip profiles/Ender3_v2_PETG/creality_* -j
 
-if [ ! -r ./profiles ]; then
-    mkdir profiles
+if [ ! -r ./dist ]; then
+    mkdir dist
 fi
 
-mv Ender3_v2_PETG.zip profiles/Ender3_v2_PETG.curaprofile
+mv Ender3_v2_PETG.zip dist/Ender3_v2_PETG.curaprofile
