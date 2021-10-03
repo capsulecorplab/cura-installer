8e4fef8c0f2d4d6f2e4f56eb1a907fb0c0f0c2c4
Sean Marquez <capsulecorplab@gmail.com>
2021-10-02 18:18:55 -0600

    add profile for CR30 printer w/ PLA+ filament

--- makeProfiles.sh
+++ makeProfiles.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-docker run --rm --volume "$PWD:/srv" -w "/srv" kramos/alpine-zip ender3_v2_petg.zip profiles/ender3_v2_petg/creality_* -j
+docker run --rm --volume "$PWD:/srv" -w "/srv" kramos/alpine-zip ender3_v2_petg.zip profiles/ender3_v2_petg/* -j
+docker run --rm --volume "$PWD:/srv" -w "/srv" kramos/alpine-zip cr30_pla+.zip profiles/cr30_pla+/* -j
 
 if [ ! -r ./dist ]; then
     mkdir dist
 fi
 
 mv ender3_v2_petg.zip dist/ender3_v2_petg.curaprofile
+mv cr30_pla+.zip dist/cr30_pla+.curaprofile
